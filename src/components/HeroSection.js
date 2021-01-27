1878b5ae4b73db23a944e3b4a65216b3845312f3
Piper <piperschwartz@aol.com>
2021-01-26 23:05:23 -0700

    testing testing

---
+++ src/components/HeroSection.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+    return (
+        <div className='hero-container'>
+            <video src='/videos/video-2.mp4' autoPlay loop muted />
+            <h1> ADVENTURE AWAITS </h1>
+            <p>What are you waiting for?</p>
+            <div className='hero-btns'>
+                <Button 
+                className='btns' 
+                buttonStyle='btn--outline'
+                buttonSize='btn--large'>
+                Get Started
+                </Button>
+                <Button 
+                className='btns' 
+                buttonStyle='btn--primary'
+                buttonSize='btn--large'>
+                Watch Trailer <i className='far fa-play-circle' />
+                </Button>
+            </div>
+        </div>
+    );
+}
+
+export default HeroSection;
